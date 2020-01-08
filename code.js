35523fdcb310f1aa7f542654b41b59a427d77da5
tohico <37986696+tohico@users.noreply.github.com>
2020-01-08 04:07:42 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -3,7 +3,6 @@
 /* ------------------------------------------------------ */
 
 maxHistory = 33;  //recommended: 1080p = 33 / 1440p = 48
-  
 emptyStartLine = true;
 
 /* ------------------------------------------------------ */
@@ -46,23 +45,18 @@ if(url.match("steamcommunity.com/market/listings")){
       newHistory[n] = [0,0,0,0];
 
     if(saleHistory[i][0].substring(0,hourIndex-2) == saleHistory[i+1][0].substring(0,hourIndex-2)){
-
         newHistory[n][1] += parseFloat(saleHistory[i][1]);
         newHistory[n][2] += parseFloat(saleHistory[i][2]); 
         newHistory[n][3] += 1;
-
     }
     else{
-
         newHistory[n][0] = saleHistory[i][0];
         newHistory[n][1] += parseFloat(saleHistory[i][1]);
         newHistory[n][2] += parseFloat(saleHistory[i][2]);  
 
         if(newHistory[n][3] != 0){
-
           newHistory[n][3] += 1;
           newHistory[n][1] = (newHistory[n][1]/(newHistory[n][3]));
-
         }
 
         if(newHistory[n][0].substring(hourIndex-2,hourIndex) != "01")
@@ -80,13 +74,10 @@ if(url.match("steamcommunity.com/market/listings")){
   dateNow.setHours(dateNow.getHours() - 24);
 
   while (i >= saleHistoryCount-1-24){
-
     if(steamDate(saleHistory[i][0]) > dateNow){
-
       last24hrs[1] += parseFloat(saleHistory[i][1]);
       last24hrs[2] += parseFloat(saleHistory[i][2]); 
       last24hrs[3] += 1;
-
     }
     i--;
   }
@@ -100,13 +91,10 @@ if(url.match("steamcommunity.com/market/listings")){
   console.log("Last 24 hours \t\t\t\t\t\t\t\t\t\t\t\t\t\t" + "[" + last24hrs[2] + "]");
 
   while (n >= newHistory.length-maxHistory) {
-
     if(newHistory[n] == undefined) break;
     console.log(steamDate(newHistory[n][0]) + "\t[" + newHistory[n][2] + "]");
     if(n == newHistory.length-1)                  console.log("========================");
     if(steamDate(newHistory[n][0]).getDay() == 1) console.log("------------------------");
     n--;
-
   }
-
 }
